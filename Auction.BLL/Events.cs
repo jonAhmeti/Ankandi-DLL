d3984b9e654bba7d591cce161e11a7ca65f18c97
jonAhmeti <63062726+jonAhmeti@users.noreply.github.com>
2020-12-15 17:05:04 +0100

    Initial commit
    Events, Items, Users, so far **NOT TESTED**

---
+++ Auction.BLL/Events.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Auction.BO;
+using Auction.BO.Interfaces;
+
+namespace Auction.BLL
+{
+    class Events : ICrud<BO.Event>
+    {
+        private readonly DAL.Events dalEvents = new DAL.Events();
+        public async Task<bool> AddAsync(Event obj)
+        {
+            return await dalEvents.AddAsync(obj);
+        }
+
+        public async Task<bool> DeleteAsync(int objId)
+        {
+            return await dalEvents.DeleteAsync(objId);
+        }
+
+        public async Task<bool> UpdateAsync(Event obj)
+        {
+            return await dalEvents.UpdateAsync(obj);
+        }
+
+        public async Task<Event> GetAsync(int objId)
+        {
+            return await dalEvents.GetAsync(objId);
+        }
+
+        public async Task<IEnumerable<Event>> GetAllAsync()
+        {
+            return await dalEvents.GetAllAsync();
+        }
+    }
+}
---
+++ Auction.BLL/Items.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Auction.BO;
+using Auction.BO.Interfaces;
+
+namespace Auction.BLL
+{
+    class Items : ICrud<BO.Item>
+    {
+        private readonly DAL.Items dalItems = new DAL.Items();
+
+        public async Task<bool> AddAsync(Item obj)
+        {
+            return await dalItems.AddAsync(obj);
+        }
+
+        public async Task<bool> DeleteAsync(int objId)
+        {
+            return await dalItems.DeleteAsync(objId);
+        }
+
+        public async Task<bool> UpdateAsync(Item obj)
+        {
+            return await dalItems.UpdateAsync(obj);
+        }
+
+        public async Task<Item> GetAsync(int objId)
+        {
+            return await dalItems.GetAsync(objId);
+        }
+
+        public async Task<IEnumerable<Item>> GetAllAsync()
+        {
+            return await dalItems.GetAllAsync();
+        }
+    }
+}
